801cfb18eccf7169083bdce55a8a1fd4f13aff99
Daniel Alexandre <liengesbor@gmail.com>
2021-11-08 22:31:05 -0800

    Created RulesetContext

---
+++ tests/Behat/Context/Ui/Admin/RulesetContext.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\SkyBoundTech\SyliusWholesalePlugin\Behat\Context\Ui\Admin;
+
+
+use Behat\Behat\Context\Context;
+
+class RulesetContext implements Context
+{
+
+}
