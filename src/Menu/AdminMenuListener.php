b8fa1d6b28c20d333dae7b5f5e44b63ffea4be5a
Daniel Alexandre <liengesbor@gmail.com>
2021-11-28 18:55:20 -0800

    Added AdminMenuListener.php

---
+++ src/Menu/AdminMenuListener.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace SkyBoundTech\SyliusWholesalePlugin\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+    public function addAdminMenuItems(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $newSubmenu = $menu
+            ->addChild('new')
+            ->setLabel('Custom Admin Submenu');
+
+        $newSubmenu
+            ->addChild('new-subitem')
+            ->setLabel('Custom Admin Menu Item');
+    }
+}
