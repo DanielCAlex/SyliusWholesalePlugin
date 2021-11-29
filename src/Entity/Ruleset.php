75f2f65cf508c47460373309158f8d2ed3e59d72
Daniel Alexandre <liengesbor@gmail.com>
2021-11-28 20:29:47 -0800

    Added WholesaleRuleset entity as Ruleset.orm.xml

---
+++ src/Entity/Ruleset.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace SkyBoundTech\SyliusWholesalePlugin\Entity;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+final class Ruleset implements RulesetInterface, ResourceInterface
+{
+    /** @var int */
+    private $id;
+    /** @var string */
+    private $name;
+    /** @var string */
+    private $description;
+    /** @var boolean */
+    private $enabled;
+
+    /**
+     * @param int $id
+     * @param string $name
+     * @param string $description
+     * @param bool $enabled
+     */
+    public function __construct(int $id, string $name, string $description, bool $enabled)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     */
+    public function setEnabled(bool $enabled): void
+    {
+        $this->enabled = $enabled;
+    }
+}
---
+++ src/Entity/RulesetInterface.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SkyBoundTech\SyliusWholesalePlugin\Entity;
+
+interface RulesetInterface
+{
+    public function getId(): int;
+
+    public function getName(): string;
+
+    public function setName(string $name): void;
+
+    public function getDescription(): string;
+
+    public function setDescription(string $description): void;
+
+    public function isEnabled(): bool;
+
+    public function setEnabled(bool $enabled): void;
+}
