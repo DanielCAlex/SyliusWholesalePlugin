f86905d3d281399d1a020f1f63bc5d1c0081183a
Daniel Alexandre <liengesbor@gmail.com>
2021-11-30 19:19:37 -0800

    Added IndexPageInterface.php for ruleset resource

---
+++ tests/Behat/Page/Admin/Ruleset/IndexPage.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\SkyBoundTech\SyliusWholesalePlugin\Behat\Page\Admin\Ruleset;
+
+use Sylius\Behat\Page\Admin\Crud\IndexPage as BaseIndexPage;
+
+class IndexPage extends BaseIndexPage implements IndexPageInterface
+{
+}
