20bd279139e12f7e982e64cfa1298f2c5c74ce73
Daniel Alexandre <liengesbor@gmail.com>
2021-11-30 21:42:43 -0800

    Added entity spec

---
+++ spec/Entity/RulesetSpec.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\SkyBoundTech\SyliusWholesalePlugin\Entity;
+
+use PhpSpec\ObjectBehavior;
+use SkyBoundTech\SyliusWholesalePlugin\Entity\Ruleset;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+class RulesetSpec extends ObjectBehavior
+{
+    function it_is_initializable(): void
+    {
+        $this->shouldHaveType(Ruleset::class);
+    }
+
+    function it_is_a_resource(): void
+    {
+        $this->shouldHaveType(ResourceInterface::class);
+    }
+
+    function it_toggles(): void
+    {
+        $this->setEnabled(true);
+        $this->isEnabled()->shouldNotReturn(false);
+        $this->isEnabled()->shouldReturn(true);
+
+        $this->setEnabled(false);
+        $this->isEnabled()->shouldNotReturn(true);
+        $this->isEnabled()->shouldReturn(false);
+    }
+
+    function it_associates_channels(ChannelInterface $firstChannel, ChannelInterface $secondChannel): void
+    {
+        $this->addChannel($firstChannel);
+        $this->hasChannel($firstChannel)->shouldReturn(true);
+
+        $this->hasChannel($secondChannel)->shouldReturn(false);
+
+        $this->removeChannel($firstChannel);
+
+        $this->hasChannel($firstChannel)->shouldReturn(false);
+    }
+}
