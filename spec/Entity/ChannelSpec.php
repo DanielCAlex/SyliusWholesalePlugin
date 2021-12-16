e81af8c5d0eabfc278058b949b6d85ad8e71a4ec
Daniel Alexandre <liengesbor@gmail.com>
2021-12-15 20:45:42 -0800

    Associated rulesets.

---
+++ spec/Entity/ChannelSpec.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\SkyBoundTech\SyliusWholesalePlugin\Entity;
+
+use PhpSpec\ObjectBehavior;
+use SkyBoundTech\SyliusWholesalePlugin\Entity\Channel;
+use SkyBoundTech\SyliusWholesalePlugin\Entity\ChannelInterface;
+use SkyBoundTech\SyliusWholesalePlugin\Entity\RulesetInterface;
+
+final class ChannelSpec extends ObjectBehavior
+{
+    public function it_is_initializable(): void
+    {
+        $this->shouldHaveType(Channel::class);
+    }
+
+    public function it_implements_channel_interface(): void
+    {
+        $this->shouldHaveType(ChannelInterface::class);
+    }
+
+    public function it_associates_rulesets(RulesetInterface $rulesetOne, RulesetInterface $rulesetTwo, RulesetInterface $rulesetThree): void
+    {
+        $this->addRuleset($rulesetOne);
+        $this->hasRuleset($rulesetOne)->shouldReturn(true);
+
+        $this->addRuleset($rulesetTwo);
+        $this->hasRuleset($rulesetTwo)->shouldReturn(true);
+
+        $this->removeRuleset($rulesetTwo);
+        $this->hasRuleset($rulesetTwo)->shouldReturn(false);
+
+        $this->hasRuleset($rulesetThree)->shouldReturn(false);
+
+    }
+}
