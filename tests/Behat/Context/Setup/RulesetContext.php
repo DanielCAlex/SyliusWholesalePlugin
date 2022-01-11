dea99c53ee501b99160660347dcb762dcf539a15
Daniel Alexandre <liengesbor@gmail.com>
2022-01-10 19:22:39 -0800

    Added interface to ruleset resource

---
+++ tests/Behat/Context/Setup/RulesetContext.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file was created by a developer at SkyBound Tech.
+ * @author Daniel Alexandre <daniel.alexandre@skyboundtech.co>
+ *
+ * (c) SkyBound Tech
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\SkyBoundTech\SyliusWholesalePlugin\Behat\Context\Setup;
+
+final class RulesetContext
+{
+
+}
